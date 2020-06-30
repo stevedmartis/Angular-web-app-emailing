fd0027743556921e719551f12f386046e9ae0bb0
Ruben Alvarado <ralvara3@externos.bancoestado.cl>
2020-06-29 21:36:37 -0400

    change endpoint con balanceador https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     hmr       : false,
-    apiUrl: 'http://ec2-18-228-221-205.sa-east-1.compute.amazonaws.com:8080'
+    apiUrl: 'https://turevento-347662686.sa-east-1.elb.amazonaws.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
     production: false,
     hmr       : false,
-    apiUrl: 'http://ec2-18-228-221-205.sa-east-1.compute.amazonaws.com:8080'
+    apiUrl: 'https://turevento-347662686.sa-east-1.elb.amazonaws.com'
 };
 
 
